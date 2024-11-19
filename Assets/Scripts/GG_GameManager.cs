482787ecc8c9d6781a4f0859f0c466b37448118e
HWANG <jhdsew9471@gmail.com>
2024-11-19 16:16:31 +0900

    no message

--- Assets/Scripts/GG_GameManager.cs
+++ Assets/Scripts/GG_GameManager.cs
@@ -5,15 +5,15 @@ using System.Collections;
 public class GG_GameManager : MonoBehaviour
 {
     [SerializeField]
-    private string username = string.Empty;
+    private string username = string.Empty; //아이디 문자열
     [SerializeField]
-    private string password = string.Empty;
+    private string password = string.Empty; // 비밀번호 문자열
 
     private const string loginUri = "https://127.0.0.1/login.php";
 
     private void Start()
     {
-        StartCoroutine(LoginCoroutine(username, password));
+        StartCoroutine(LoginCoroutine(username, password)); //아이디 비밀번호 받아서 로그인 코루틴 시작
     }
     private IEnumerator LoginCoroutine(string _username, string _password)
     {
