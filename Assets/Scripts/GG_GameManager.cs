1764eaf2a606b6daf4eaa47e63f023315c2c071c
HWANG <jhdsew9471@gmail.com>
2024-11-20 10:57:56 +0900

    id 중복체크 GG_Gamemanager 작성

--- Assets/Scripts/GG_GameManager.cs
+++ Assets/Scripts/GG_GameManager.cs
@@ -30,9 +30,9 @@ public class GG_GameManager : MonoBehaviour
         //StartCoroutine(LoginCoroutine(id)); //아이디 비밀번호 받아서 로그인 코루틴 시작
 
         UImg.onClickSubmitBtn = Signin_Infos; // Signin버튼을 UI 에서 누르면 대리자가 Signin_Infos 를 실행하게 설정함 
-        //UImg.OnClickBtn=   //중복방지 버튼을  UI에서 누르면 대리자가 중복방지를 실행함
+        UImg.onClickIDCheckBtn = SameIdCheck;  //중복방지 버튼을  UI에서 누르면 대리자가 중복방지를 실행함
     }
-    
+
     private void Signin_Infos() //들어온 정보들을 가지고 코루틴(SigninCoroutine)을 시작 
     {
         id = UImg.Id;
@@ -46,6 +46,26 @@ public class GG_GameManager : MonoBehaviour
             Debug.Log("Id 중복확인 후 회원가입 ");
         }
     }
+    private void SameIdCheck()
+    {
+        if (isDifferentId == true)
+            //나중에 아이디에 적힌값이 바꼈을때 다시 
+            //onValuechanged 등 통해 isDifferentId를 false로 바꿀것 안그러면 중복방지체크후 아이디를 바꾼 후 회원가입할 수 있음 11.20
+        {
+            Debug.Log("아이디 생성 가능(중복된 게 없음)");
+            return;
+        }
+        if (isDifferentId == false)
+        {
+            id = UImg.Id; 
+            StartCoroutine(SameIdCheckCoroutine(id));// 코루틴 안에서 중복된지 확인해서 안에서 isDifferentId를 false나 true로 바꿔줌
+            if (isDifferentId == true)
+                Debug.Log("아이디 생성 가능(중복된 게 없음)");
+            else
+                Debug.Log("아이디 생성 불가능(중복)");
+        }
+    }
+
 
     private IEnumerator SigninCoroutine(string _id/*, string _password*/) //가져온 정보들을 서버에 전달 추가, 회원가입 코루틴 
     {
