865ba94dfd4d38430851d3c2c32d253ba2c0ab4e
HWANG <jhdsew9471@gmail.com>
2024-11-20 10:43:29 +0900

    GG_Gamemanger Update

--- Assets/Scripts/GG_GameManager.cs
+++ Assets/Scripts/GG_GameManager.cs
@@ -6,7 +6,7 @@ using Newtonsoft.Json;
 
 public class GG_GameManager : MonoBehaviour
 {
-    private bool sameId = false;
+    private bool isDifferentId = false;
 
     [SerializeField]
     private LoginUIManager UImg = null;
@@ -19,7 +19,7 @@ public class GG_GameManager : MonoBehaviour
     [SerializeField]
     private string birthday = string.Empty; // 생일 문자열
     [SerializeField]
-    private string AR_Q = string.Empty; // 복구질문 문자열
+    private int AR_Q = 0; // 복구질문번호 정수형
     [SerializeField]
     private string AR_A = string.Empty; // 복구답변 문자열
 
@@ -29,24 +29,51 @@ public class GG_GameManager : MonoBehaviour
     {
         //StartCoroutine(LoginCoroutine(id)); //아이디 비밀번호 받아서 로그인 코루틴 시작
 
-        UImg.onClickSubmitBtn = Signin_Infos; // 버튼을 누르면 대리자가 Signin_Infos 를 실행하게 설정함 
+        UImg.onClickSubmitBtn = Signin_Infos; // Signin버튼을 UI 에서 누르면 대리자가 Signin_Infos 를 실행하게 설정함 
+        //UImg.OnClickBtn=   //중복방지 버튼을  UI에서 누르면 대리자가 중복방지를 실행함
     }
     
-    private void Signin_Infos() //들어온 정보들을 가지고 코루틴(SigninCoroutine)을 시작
+    private void Signin_Infos() //들어온 정보들을 가지고 코루틴(SigninCoroutine)을 시작 
     {
         id = UImg.Id;
         Debug.Log("id:" + id);
-
-        StartCoroutine(SigninCoroutine(id/*,password*/));
+        if (isDifferentId == true)
+        {
+            StartCoroutine(SigninCoroutine(id/*,password*/));
+        }
+        else
+        {
+            Debug.Log("Id 중복확인 후 회원가입 ");
+        }
     }
 
-    private IEnumerator SigninCoroutine(string _id/*, string _password*/) //회원가입 코루틴
+    private IEnumerator SigninCoroutine(string _id/*, string _password*/) //가져온 정보들을 서버에 전달 추가, 회원가입 코루틴 
     {
         WWWForm form = new WWWForm(); //서버 전달 형태를 정함
-
+        form.AddField("Id", _id);
         //form.AddField("Password", _password);
         //웹서버는 비동기 방식
         using (UnityWebRequest www = UnityWebRequest.Post(loginUri, form)) //post는 보안 //get은 속도
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.ConnectionError ||
+                www.result == UnityWebRequest.Result.DataProcessingError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                //Debug.Log(www.downloadHandler.text);
+            }
+        }
+    }
+
+    private IEnumerator SameIdCheckCoroutine(string _id) //아이디 중복방지 코루틴
+    {
+        WWWForm form = new WWWForm(); //서버 전달 형태를 정함
+        form.AddField("id", _id); //서버에 id를 넘겨줌
+        using (UnityWebRequest www = UnityWebRequest.Post(loginUri, form)) 
         {
             yield return www.SendWebRequest();
 
@@ -58,28 +85,15 @@ public class GG_GameManager : MonoBehaviour
             else
             {
                 Debug.Log(www.downloadHandler.text);
-            }
-        }
-    }
-    private IEnumerator LoginCoroutine(string _id/*, string _password*/) //로그인 코루틴
-    {
-        WWWForm form = new WWWForm(); //서버 전달 형태를 정함
-
-        //form.AddField("Password", _password);
-        //웹서버는 비동기 방식
-        using (UnityWebRequest www = UnityWebRequest.Post(loginUri, form)) //post는 보안 //get은 속도
-        {
-            yield return www.SendWebRequest();
-
-            if (www.result == UnityWebRequest.Result.ConnectionError ||
-                www.result == UnityWebRequest.Result.DataProcessingError)
-            {
-                Debug.Log(www.error);
-            }
-            else
-            {
-                 Debug.Log(www.downloadHandler.text);
-                 AR_A = www.downloadHandler.text;
+                string CheckId = www.downloadHandler.text; //서버에서 다른지 같은지 문자열로 알려줌
+                if (CheckId == "false") //같은게 있으면 false로 답변 해주기로함
+                {
+                    isDifferentId = false;
+                }
+                else
+                {
+                    isDifferentId = true;
+                }
             }
         }
     }
