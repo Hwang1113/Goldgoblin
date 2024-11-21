9c1a6bcb7bd2a1fc1613f09639c29d13cf2748c2
DESKTOP-9T83A1V\admin <kns7208@naver.com>
2024-11-21 11:22:24 +0900

    item php 추가함

---
+++ item.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "game_db";
+
+$itemNum = $_POST["ItemNumber"];
+
+$conn = new mysqli($servername,
+								 $username,
+								 $password,
+								 $dbname);
+
+$sql = "SELECT * FROM tb_item WHERE ItemNumber = '".$itemNum."'";
+//$Insertsql = "INSERT INTO tb_sign(Id, Password, Birthday, AR_Q, AR_A)
+		//VALUES('".$loginUser."','".$UserPassword."','".$UserNickname."','".$UserAR_Q."','".$UserAR_A."')";
+$result = $conn->query($sql);
+
+ if($result->num_rows > 0 ){
+    echo "[";
+    while($row = $result->fetch_assoc())
+        {
+            echo "{'ItemNumber': '".$row['ItemNumber']."', 'Name': '" .$row['Name']. "', 'Des': '".$row['Des']."', 'Rarity': '".$row['Rarity']."', 'Icon': '".$row['Icon']."'}";
+        }
+    echo "]";
+ }
+
+ $conn->close();
+?>
