5088a8d822b94cd2e1f054d77782f79856cf07df
DESKTOP-9T83A1V\admin <kns7208@naver.com>
2024-11-21 15:18:15 +0900

    getinven php추가, 인벤토리 아이템 명령어 txt파일 추가

---
+++ getinven.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "";
+$dbname = "game_db";
+
+$UserId = $_POST["Id"];
+
+$conn = new mysqli($servername,
+								 $username,
+								 $password,
+								 $dbname);
+
+$sql = "SELECT * FROM tb_inven WHERE Id = '".$UserId."'";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0 ){
+    while($row = $result->fetch_assoc())
+    echo $row["ItemSlot"];    
+    // $row = $result->fetch_assoc()
+    // echo $row["ItemSlot"];
+}
+else
+    echo 0;
+
+ $conn->close();
+?>
